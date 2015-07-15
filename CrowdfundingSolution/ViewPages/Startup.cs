6c921b766412a4b8b4222dae70263980660946cf
huacker <150974689@qq.com>
2015-07-15 09:47:45 +0800

    create

---
+++ CrowdfundingSolution/ViewPages/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ViewPages.Startup))]
+namespace ViewPages
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
