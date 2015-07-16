cc054b8951af6220a8989ca788fd7dd2d1559cc1
huacker <150974689@qq.com>
2015-07-16 11:19:39 +0800

    添加筹资计划内容页面

---
+++ CrowdfundingSolution/ViewPages/Controllers/CrowdfundingPlanController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using ViewPages.Models;
+using CrowdfundingSolution.Base;
+using CrowdfundingSolution.BLLObject;
+
+namespace ViewPages.Controllers
+{
+    public class CrowdfundingPlanController: BaseController
+    {
+        public CrowdfundingPlanController()
+        {
+
+        }
+
+        public ViewResult Add()
+        {
+            CrowdfundingPlan plan = new CrowdfundingPlan();
+            plan.Add();
+            return View();
+        }
+    }
+}
