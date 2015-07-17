bdeeb0a2a7c07fdd8631566bb0a00f77c55d5f17
lixinhua <ListVip@yeah.net>
2015-07-17 18:03:49 +0800

    项目发布页面数据保存

---
+++ CrowdfundingSolution/BLLObject/CrowdReleaseBLL.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System;
+using CrowdfundingSolution.BLLInterface;
+
+
+namespace CrowdfundingSolution.BLLObject
+{
+    public class CrowdReleaseBLL : ICrowdRelease
+    {
+        public void ReleaseAdd(Dictionary<string, string> data)
+        {
+            using (var entity = new DAO.CrowdfundingSolutionEntities())
+            {
+                var bs = new DAO.Basic();
+                bs.ID = Guid.NewGuid().ToString();
+                bs.ProjectName = data["ShortAlias"];
+                bs.Synopsis = data["CrowdfundingPlan"];
+                bs.IndustryID = data["CrowdfundingAmount"];
+                bs.AreaID = data["Percentage"];
+
+                entity.Basic.Add(bs);
+                entity.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CrowdfundingSolution/ViewPages/Controllers/CrowdDetailsControllers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using ViewPages.Models;
+using CrowdfundingSolution.Base;
+using CrowdfundingSolution.BLLInterface;
+using CrowdfundingSolution.BLLObject;
+
+namespace ViewPages.Controllers
+{
+    public class CrowdDetailsControllers : BaseController
+    {
+        public CrowdDetailsControllers()
+        {
+
+        }
+
+        public ViewResult DetailsAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DetailsAdd(FormCollection coll)//，CrowdfundingAmount，Percentage，Prerelease，BegingDate,EndDate
+        {
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            data["ShortAlias"] = coll["ShortAlias"];
+            data["CrowdfundingPlan"] = coll["CrowdfundingPlan"];
+            data["CrowdfundingAmount"] = coll["CrowdfundingAmount"];
+            data["Percentage"] = coll["Percentage"];
+            data["Prerelease"] = coll["Prerelease"];
+            data["BegingDate"] = coll["BegingDate"];
+            data["EndDate"] = coll["EndDate"];
+            ICrowdfundingPlan plan = new CrowdfundingPlan();
+            plan.Add(data);
+            return View();
+        }
+    }
+}
